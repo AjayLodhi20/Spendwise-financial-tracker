2b85bce1ee6d7c0ee9e814844740289c045d457a
Ajay lodhi <satyamlodhi123@gmail.com>
2026-04-22 22:49:46 +0530

    code is longer

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ CATEGORIES = ('Food', 'Transport', 'Entertainment', 'Utilities', 'Misc')
 numbers = '1234567890'
 numbers = list(numbers)
 
-def account_no(acc_name)
+def account_no(acc_name):
+    acc_holder= {}
+    if isinstance(acc_name, str):
+        acc_name = acc_name.upper()
+        random.shuffle(numbers)
+        new_list = "".join(numbers)
+        acc = f"{acc_name}{new_list}"
+        return acc_holder[acc_name]
+    else:
+        raise AttributeError("write string in the function call..")
+
+print(account_no('satyam'))
+
 # show some info when i write the account name..like account no. name date of account open
 # store all user data in a dictionary form
 # check if the user has put one of the existing name correctly
